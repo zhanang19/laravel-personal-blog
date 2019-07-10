f48d085e131e017f478039343781f4206e3f6f79
Zainal Hasan <zhanang19@gmail.com>
2019-07-10 21:52:30 +0700

    add avatar on navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,6 +49,7 @@
                                 </li>
                             @endif
                         @else
+                            <img src="{{ asset('storage/images/' . Auth::user()->avatar) }}" style="display:inline-block;width:37px;height:37px;margin-right:10px" alt="">
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
