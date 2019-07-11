87bfd72f8e9ae2e336828fe37b5a93e67c5f5cca
Zainal Hasan <zhanang19@gmail.com>
2019-07-11 11:02:59 +0700

    add migration
    migration file to create categories, posts and comments table

---
+++ database/migrations/2019_07_11_031749_create_posts_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('slug')->unique();
+            $table->string('title');
+            $table->text('content');
+            $table->string('photo')->default('post.png');
+            $table->bigInteger('views');
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
