9930bdc4a5df5e902af3ba0419470a6e95a03ac1
Zainal Hasan <zhanang19@gmail.com>
2019-07-11 11:48:20 +0700

    change homepage view

---
+++ app/Http/Controllers/FrontpageController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use App\Category;
+use Illuminate\Http\Request;
+
+class FrontpageController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::limit(4)->get();
+        $categories = Category::limit(5)->get();
+        return view('welcome', compact('posts', 'categories'));
+    }
+}
---
+++ resources/views/components/blog-card.blade.php
@@ -0,0 +1,14 @@
+<div class="col-md-4 my-3">
+    <div class="card text-center">
+        <div class="card-header">
+            <h4>{{ \Str::limit($data->title, 20) }}</h4>
+            <span class="badge badge-primary">{{ $data->category->name }}</span>
+        </div>
+        <div class="card-body">
+            <img class="img-fluid" style="max-height:200px" src="{{ asset('storage/images/' . $data->photo) }}" alt="">
+        </div>
+        <div class="card-footer">
+            <a href="{{ url('/post/' . $data->slug) }}" class="btn btn-primary">More Detail</a>
+        </div>
+    </div>
+</div>
