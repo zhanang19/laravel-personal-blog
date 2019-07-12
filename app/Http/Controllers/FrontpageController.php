f97e456849defde1402588b5fb6e52f62b6d6b50
Zainal Hasan <zhanang19@gmail.com>
2019-07-12 20:20:55 +0700

    use Auth facade

--- app/Http/Controllers/FrontpageController.php
+++ app/Http/Controllers/FrontpageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
 use App\Post;
 use App\Comment;
 use App\Category;
--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
 use App\Post;
 use App\Category;
 use Illuminate\Http\Request;
