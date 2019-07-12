3d3fc93ce22df7e6717333b40a56d31ecb44c9a8
Zainal Hasan <zhanang19@gmail.com>
2019-07-12 14:31:24 +0700

    add comment empty view and refactor css code

--- resources/views/components/blog-empty.blade.php
+++ resources/views/components/blog-empty.blade.php
@@ -1,7 +1,5 @@
 <div class="col-md-12 my-3">
-    <div class="card text-center">
-        <div class="card-body">
-            <h4>No post found</h4>
-        </div>
+    <div class="card">
+        <div class="card-body text-center h4">No post found.</div>
     </div>
 </div>
---
+++ resources/views/components/comment-empty.blade.php
@@ -0,0 +1,5 @@
+<div class="col-md-12 my-3">
+    <div class="card">
+        <div class="card-body p-3 pb-0 text-center h4">No comments for this post.</div>
+    </div>
+</div>
