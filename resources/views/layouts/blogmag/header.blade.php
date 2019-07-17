b16f5afa960290aff4a9f0cb4084afc0c1065dae
Zainal Hasan <zhanang19@gmail.com>
2019-07-17 19:55:09 +0700

    add blogmag template

---
+++ resources/views/layouts/blogmag/header.blade.php
@@ -0,0 +1,17 @@
+<!-- Header -->
+<header>
+    <div class="upper-head clearfix">
+        <div class="container">
+            <div class="header-date">
+                <p><i class="icon-clock"></i> Thursday, July 19, 2018</p>
+                <p><i class="icon-cloud"></i> Hollywood, America</p>
+            </div>
+            <ul class="header-social-links pull-right">
+                <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+            </ul>
+        </div>
+    </div>
+</header>
+<!-- Header Ends -->
---
+++ resources/views/layouts/blogmag/navbar.blade.php
@@ -0,0 +1,113 @@
+<!-- Navigation Bar -->
+<div class="navigation">  
+    <div class="container">
+        <div class="navigation-content">
+            <div class="header_menu">
+                <!-- start Navbar (Header) -->
+                <nav class="navbar navbar-default navbar-sticky-function navbar-arrow">
+                    <div class="logo pull-left">
+                        <a href="index.html"><img alt="Image" src="template/images/logo1.png"></a>
+                    </div>
+                    <div id="navbar" class="navbar-nav-wrapper pull-right">
+
+                        <ul class="nav navbar-nav" id="responsive-menu">
+
+                            <li class="active has-child">
+                                <a href="#">Home <i class="fa fa-angle-down"></i></a>
+                                <ul>
+                                    <li><a href="#">Magazine</a>
+                                        <ul>
+                                            <li><a href="index.html">Style 1</a></li>
+                                            <li><a href="index-magazine-2.html">Style 2</a></li>
+                                            <li><a href="index-magazine-dark.html">Style 3</a></li>
+                                            <li><a href="index-magazine-boxed.html">Style 4</a></li>
+                                            <li><a href="index-beauty.html">Beauty</a></li>
+                                            <li><a href="index-sports.html">Sports</a></li>
+                                            <li><a href="index-business.html">Business</a></li>
+                                            <li><a href="index-tech.html">Technology</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="#">Blog</a>
+                                        <ul>
+                                            <li><a href="index-blog.html">Style 1</a></li>
+                                            <li><a href="index-blog-2.html">Style 2</a></li>
+                                            <li><a href="index-blog-3.html">Style 3</a></li>
+                                            <li><a href="index-blog-4.html">Style 4</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="has-child">
+                                <a href="#">Pages <i class="fa fa-angle-down"></i></a>
+                                <ul>
+                                    <li><a href="#">Blog List Category</a>
+                                        <ul>
+                                            <li><a href="list-rightsidebar.html">Right Sidebar</a></li>
+                                            <li><a href="list-leftsidebar.html">Left Sidebar</a></li>
+                                            <li><a href="list.html">No Sidebar</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="#">Blog Grid Category</a>
+                                        <ul>
+                                            <li><a href="list-gridview.html">3 Column</a></li>
+                                            <li><a href="list-gridview-2.html">2 Column</a></li>
+                                            <li><a href="list-gridview-3.html">Single Column</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="#">Detail Page</a>
+                                        <ul>
+                                            <li><a href="detail.html">Detail Page 1</a></li>
+                                            <li><a href="detail-fullwidth.html">Detail Page 2</a></li>
+                                            <li><a href="detail1.html">Detail Page 3</a></li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a href="detail-default.html">Default Page</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="has-child">
+                                <a href="#">Shop <i class="fa fa-angle-down"></i></a>
+                                <ul>
+                                    <li><a href="shop.html">Shop List</a></li>
+                                    <li><a href="store-detail.html">Shop Single</a></li>
+                                    <li><a href="login.html">Account</a></li>
+                                    <li><a href="cart.html">Cart</a></li>
+                                    <li><a href="checkout.html">Checkout</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="detail.html">Sports</a>
+                            </li>                                
+                            <li>
+                                <a href="detail.html">Fashion</a>
+                            </li>
+                            <li>
+                                <a href="detail.html">Politics</a>
+                            </li>
+                            <li>
+                                <a href="about-us.html">About Us</a>                                   
+                            </li>
+                            <li>
+                                <a href="contact-us.html">Contact Us</a>
+                            </li>
+                            <li>
+                                <a id="searchtoggl" class="searchtoggle"><i class="fa fa-search" aria-hidden="true"></i></a>
+                            </li>
+                        </ul>
+                    </div><!--/.nav-collapse -->
+                    <div id="slicknav-mobile"></div>
+                </nav>
+            </div>
+            <div id="searchbar" class="searchbar">
+                <form>
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="search" placeholder="Search Now">
+                        <a href="#"><span class="search_btn"><i class="fa fa-search" aria-hidden="true"></i></span></a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Navigation Bar Ends -->
